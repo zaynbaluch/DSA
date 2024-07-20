ae7ad3a2196ca91a8af67e23a753cfcec8a8555e
Zayn <mzainabbas77@gmail.com>
2024-07-20 16:14:02 +0500

    binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
-int bin_search(int req_arr[], int req_num){
-    
-    int arr_size = sizeof(req_arr)/sizeof(int);
-
-    for (int i = 0; i <= arr_size; i++){
-      for (int j = 1; i <= arr_size; i++)
-      {
-        if req_arr[]
-      };
-        
-    };
-    
+int bin_search(int req_arr[], int req_num, int n){ 
+    int start_point{};
+    int end_point = n;
+    while(start_point <= end_point){
+        int mid = ((start_point+end_point)/2);
+        if (req_arr[mid] == req_num){
+            std::cout << "Found at index " << mid << std::endl;
+            return mid;
+            
+        }
+        else if (req_arr[mid] > req_num){
+            end_point = mid-1;
+        }
+        else{
+            start_point = mid+1;
+        }
+    }
+    return -1;
 };
 
 int main(){
-    int array[10] = {2,19,12,15,5,8,9,4,1,11};
-
+    int array[10] = {1,2,4,5,9,12,15,17,19,20};
+    bin_search(array, 1, 10);
 }
